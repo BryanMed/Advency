46d0eb1d49abd541607fcd36403c583513805501
Bryan Medina <bryan.medina.per@gmail.com>
2022-12-01 10:40:38 -0600

    upload day1 advency

---
+++ Day1/src/components/Todo.jsx
@@ -0,0 +1,12 @@
+export default function Todo() {
+  return (
+    <>
+      <h1>Regalos:</h1>
+      <ul>
+        <li>Calcetas</li>
+        <li>Calcetines</li>
+        <li>Medias</li>
+      </ul>
+    </>
+  );
+}
