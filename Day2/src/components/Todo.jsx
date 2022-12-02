caf27deb6aaaf6f989f06e874915466fc5615e45
Bryan Medina <bryan.medina.per@gmail.com>
2022-12-02 10:29:01 -0600

    add styling, day 2

---
+++ Day2/src/components/Todo.jsx
@@ -0,0 +1,12 @@
+export default function Todo() {
+  return (
+    <div className="todo">
+      <h2>Regalos:</h2>
+      <ul>
+        <li>Calcetas</li>
+        <li>Plantas</li>
+        <li>Papel</li>
+      </ul>
+    </div>
+  );
+}
